4206969714fd64b81e8f6e4c068a702f894d9d99
David8633 <davidinstituto282@gmail.com>
2025-04-01 09:39:50 +0200

    actv2

---
+++ actividad2.js
@@ -0,0 +1,7 @@
+/*Genera un número aleatorio entre 0 y 100 redondeando y muéstralo por consola*/
+Math.round(Math.random() * 100)
+/*Muestra por consola el valor de PI.*/
+Math.PI
+/*Genera un número aleatorio entre 5 y 10 redondeando y muéstralo por consola. Para  este caso lo recomendado es utilizar la fórmula (Math.random()* (max-min)+min).*/
+/*Comprueba el funcionamiento del método sign(). */
+Math.sign(Math.random() * -1)
